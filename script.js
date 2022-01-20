968bc4cb1c815d1b28c2839e8b01d5d4838a1452
Nofal Sher <nofalsher7@gmail.com>
2022-01-21 01:03:58 +0500

    completed

--- script.js
+++ script.js
@@ -53,12 +53,48 @@ updateDOM();
 function doubleMoney(){
     //map will run function at every index of existing array and will get output and makes its array
     data=data.map(user=>{   //overwriting previous data
-        return {...user}        //... is called as spread operator. By it out input will as it is comes at output
+        return {...user,balance:user.balance*2}        //... is called as spread operator. By it out input will as it is comes at output
         
-        //Update DOM using new user data array
-        updateDOM();
     });
+    //Update DOM using new user data array
+    updateDOM();
 }
+
+
+//Function to filter only Millionaires
+
+function filterUsers(){
+data=data.filter(user=>user.balance >=1000000);      // It is iterative means run for every value and gives output based on our conditions
+//Update DOM function
+updateDOM();
+}
+
+//Function to sort users by balance
+function sortByBal(){
+    //Simple sort function will sort realting to string but if we want to sort related to number then we have to make an other function inside sort function.
+    data=data.sort((a,b)=>a.balance - b.balance);
+    
+    updateDOM();
+}
+
+
+//Function to sum all users balance
+function totalBalance(){
+    //Update DOM function
+    updateDOM();     // Just run it so that it does not give total value again and again
+
+    //reduce method has two inputs. accumulator and current value
+    const balance= data.reduce((acc,user)=>(acc+=user.balance),0);
+    //create a div where we will show balance
+    const balanceElement=document.createElement('div')
+    //    Set inner HTML for new div
+    balanceElement.innerHTML=`<h3>Total Balance: ${formatNumberToDollar(balance)}</h3>`
+
+    //Append balance in main element
+    main.appendChild(balanceElement)
+   
+}
+
 
 
 
@@ -91,6 +127,15 @@ addUserBtn.addEventListener('click',getRandomUser)
 
 //2.Listen for click on double btn
 doubleBtn.addEventListener('click',doubleMoney)
+
+//3. Event Listener for click on filter button
+filterBtn.addEventListener('click',filterUsers);
+
+//4. Listen for click on sort button
+sortBtn.addEventListener('click',sortByBal)
+
+//5. Listen for click on sum button
+sumBtn.addEventListener('click',totalBalance)
 
 getRandomUser();
 getRandomUser();
